a39f47ba57e1645c85e5a1454a3838f20a574a46
Vinícius Silva <vivisec007@gmail.com>
2021-09-08 09:49:52 -0300

    Criação sistema de conta

---
+++ BankOO/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace BankOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDoVinicius = new ContaCorrente();
+
+            contaDoVinicius.titular = "Vinicius";
+            contaDoVinicius.agencia = 863;
+            contaDoVinicius.conta = 863146;
+            contaDoVinicius.saldo = 100;
+
+            ContaCorrente contaDoJoao = new ContaCorrente();
+
+            contaDoJoao.titular = "João";
+            contaDoJoao.agencia = 357;
+            contaDoJoao.conta = 357135;
+            contaDoJoao.saldo = 120;
+
+            Console.WriteLine("O titular da conta é: " +contaDoVinicius.titular);
+            Console.WriteLine("O titular da conta é: " +contaDoJoao.titular);
+        }
+    }
+}
