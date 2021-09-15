2e9b8adf8ec4f0687d02e04da05b2bc496b06236
Vinícius Silva <vivisec007@gmail.com>
2021-09-15 09:36:45 -0300

    Atualização transferências

--- BankOO/Program.cs
+++ BankOO/Program.cs
@@ -1,17 +1,24 @@
 ﻿using System;
 
-namespace BankOO
+namespace myBank
 {
     class Program
     {
         static void Main(string[] args)
         {
-            ContaCorrente contaDoVinicius = new ContaCorrente("Vinícius", 863, 863146, 100);
+            ContaCorrente contaDoVinicius = new ContaCorrente("Vinicius", 1234, 789, 100.50);
+            ContaCorrente contaDoJoao = new ContaCorrente("Joao", 1234, 790, 120);
+            ContaCorrente contaDaMaria = new ContaCorrente("Maria", 1234, 791, 150);
 
-            ContaCorrente contaDoJoao = new ContaCorrente("João", 357, 357135, 120);
+            contaDoVinicius.Sacar(50);
+            Console.WriteLine("O saldo da conta da Vinicius após o saque é: " + contaDoVinicius.saldo);
 
-            Console.WriteLine("O titular da conta é: " +contaDoVinicius.titular);
-            Console.WriteLine("O titular da conta é: " +contaDoJoao.titular);
+            contaDoVinicius.Depositar(100);
+            Console.WriteLine("O saldo da conta do Vinicius após o depósito é: " + contaDoVinicius.saldo);
+
+            contaDoVinicius.Transferir(100, contaDaMaria);
+            Console.WriteLine("O saldo da conta do Vinicius após a transferência é: " + contaDoVinicius.saldo);
+            Console.WriteLine("O saldo da conta da Maria após a transferência é: " + contaDaMaria.saldo);
         }
     }
 }
