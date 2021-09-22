3f3293796bb06162b124631685bf3c760f46ca62
Vinícius Silva <vivisec007@gmail.com>
2021-09-22 09:50:11 -0300

    Polimento de funções

--- BankOO/ContaCorrente.cs
+++ BankOO/ContaCorrente.cs
@@ -1,34 +1,43 @@
 public class ContaCorrente{
-    public string titular; 
-    public int agencia;
-    public int conta; 
-    public double saldo;
+    public string Titular { get; set; }
+    public int Agencia { get; set; }
+    public int Conta { get; set; }
+    private double _saldo;
+    public double Saldo { get{
+        return this._saldo;
+    }
+    set{
+        if(value >= 0){
+            this._saldo = value;
+        }
+     } 
+    }
 
     public ContaCorrente() {}
     public ContaCorrente(string contacorrente_titular, int contacorrente_agencia, int contacorrente_conta, double contacorrente_saldo)
     { 
-        titular = contacorrente_titular;
-        agencia = contacorrente_agencia;
-        conta = contacorrente_conta;
-        saldo = contacorrente_saldo;
+        Titular = contacorrente_titular;
+        Agencia = contacorrente_agencia;
+        Conta = contacorrente_conta;
+        Saldo = contacorrente_saldo;
     }
 
     public bool Sacar (double valor){
-        if(this.saldo < valor){
+        if(this.Saldo < valor){
             return false;
         }
         else{
-            this.saldo -= valor;
+            this.Saldo -= valor;
             return true;
         }
     }
 
     public void Depositar(double valor){
-        this.saldo += valor;
+        this.Saldo += valor;
     }
 
     public bool Transferir(double valor, ContaCorrente contaDestino){
-        if(this.saldo < valor){
+        if(this.Saldo < valor){
             return false;
         }
         else{
@@ -36,5 +45,9 @@ public class ContaCorrente{
             contaDestino.Depositar(valor);
             return true;
         }
+    }
+
+    public double ConsultaSaldo(){
+        return this.Saldo;
     }
 }
--- BankOO/Program.cs
+++ BankOO/Program.cs
@@ -11,14 +11,14 @@ namespace myBank
             ContaCorrente contaDaMaria = new ContaCorrente("Maria", 1234, 791, 150);
 
             contaDoVinicius.Sacar(50);
-            Console.WriteLine("O saldo da conta da Vinicius após o saque é: " + contaDoVinicius.saldo);
+            Console.WriteLine("O saldo da conta da Vinicius após o saque é: " + contaDoVinicius.Saldo);
 
-            contaDoVinicius.Depositar(100);
+            /* contaDoVinicius.Depositar(100);
             Console.WriteLine("O saldo da conta do Vinicius após o depósito é: " + contaDoVinicius.saldo);
 
             contaDoVinicius.Transferir(100, contaDaMaria);
             Console.WriteLine("O saldo da conta do Vinicius após a transferência é: " + contaDoVinicius.saldo);
-            Console.WriteLine("O saldo da conta da Maria após a transferência é: " + contaDaMaria.saldo);
+            Console.WriteLine("O saldo da conta da Maria após a transferência é: " + contaDaMaria.saldo); */
         }
     }
 }
