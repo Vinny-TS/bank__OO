fc3d72657ee4fa84a432ddf067587609313c0053
Vinícius Silva <vivisec007@gmail.com>
2021-10-06 08:32:16 -0300

    Correção bonificação

--- BankOO/Funcionario.cs
+++ BankOO/Funcionario.cs
@@ -4,12 +4,27 @@ public class Funcionario{
     public string Cargo { get; set; }
     public double Salario { get; set; }
     public double Comissao { get; set; }
+    private double _bonificacao;
+    public double Bonificacao { 
+        get{
+            return _bonificacao;
+        } 
+        set{
+            if(this.Cargo == "Vendedor"){
+                _bonificacao += value * 0.02;
+            }
+            if(this.Cargo == "Diretor"){
+                _bonificacao += value * 0.08;
+            }  
+        } 
+    }
 
-    public Funcionario(string funcionario_nome, string funcionario_cpf, string funcionario_cargo, double funcionario_salario)
+    public Funcionario(string funcionario_nome, string funcionario_cpf, string funcionario_cargo, double funcionario_salario, double funcionario_bonificacao)
     { 
         Nome = funcionario_nome;
         CPF = funcionario_cpf;
         Cargo = funcionario_cargo;
         Salario = funcionario_salario;
+        Bonificacao = funcionario_bonificacao;
     }
 }
--- BankOO/Program.cs
+++ BankOO/Program.cs
@@ -6,17 +6,22 @@ namespace myBank
     {
         static void Main(string[] args)
         {
-            Funcionario funcionarioVinicius = new Funcionario("Vinicius Telles", "123.123.123-45", "Caixa", 1200);
-            Funcionario funcionarioCarlos = new Funcionario("Carlos", "123.456.789-10", "Caixa", 1200);
-            Funcionario funcionarioDaniel = new Funcionario("Daniel", "321.654.987-01", "Caixa", 1200);
+            Funcionario funcionarioVinicius = new Funcionario("Vinicius Telles", "123.123.123-45", "Diretor", 1200, 1);
+            Funcionario funcionarioCarlos = new Funcionario("Carlos", "123.456.789-10", "Vendedor", 1200, 1);
+            Funcionario funcionarioDaniel = new Funcionario("Daniel", "321.654.987-01", "Vendedor", 1200, 1);
 
 
             ContaCorrente contaDoCharles = new ContaCorrente("Charles", 1234, 789, 100, funcionarioVinicius);
             ContaCorrente contaDoGeorge = new ContaCorrente("George", 1234, 790, 120, funcionarioDaniel);
             ContaCorrente contaDoValtteri = new ContaCorrente("Valtteri", 1234, 791, 150, funcionarioCarlos);
 
-            Console.WriteLine("O total de contas criadas é de: " + ContaCorrente.TotalDeContasCriadas + " conta(s).");
 
+            funcionarioCarlos.Bonificacao = funcionarioCarlos.Salario;
+            funcionarioDaniel.Bonificacao = funcionarioDaniel.Salario;
+            funcionarioVinicius.Bonificacao = funcionarioVinicius.Salario;
+
+            Console.WriteLine("Bonificação: R$" + funcionarioVinicius.Bonificacao);
+            Console.WriteLine("O total de contas criadas é de: " + ContaCorrente.TotalDeContasCriadas + " conta(s).");
             Console.WriteLine("Saldo inicial: Charles R$" + contaDoCharles.Saldo + "; conta vendida pelo vendedor: " + funcionarioVinicius.Nome + ", que recebeu uma comissão de: R$" + funcionarioVinicius.Comissao);
             Console.WriteLine("Saldo inicial: George R$" + contaDoGeorge.Saldo + "; conta vendida pelo vendedor: " + funcionarioDaniel.Nome + ", que recebeu uma comissão de: R$" + funcionarioDaniel.Comissao);
             Console.WriteLine("Saldo inicial: Valtteri R$" + contaDoValtteri.Saldo + "; conta vendida pelo vendedor: " + funcionarioCarlos.Nome + ", que recebeu uma comissão de: R$" + funcionarioCarlos.Comissao);
